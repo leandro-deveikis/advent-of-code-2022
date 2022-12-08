3527111acdcb4c3e6b71917b4930fddd5104ec1a
Leandro Deveikis <leandro.deveikis@gmail.com>
2022-12-08 17:31:06 -0300

    Day03 completed

--- Day02/main.go
+++ Day02/main.go
@@ -12,25 +12,6 @@ func main() {
 }
 
 func challenge01() {
-	// Win: 6 - Draw: 3 - Lost: 0
-	// Rock (A - X): 1 - Paper (B - Y): 2 - Scissors (C - Z): 3
-	// As this has only 9 combinations, we just precalculate them and store it into a matrix.
-	// we could do something even better, intead of having this map, having a function that calculates them
-	// according to the rules, so is not that hardcoded
-	cheatsheet := map[string]int{
-		"A X": 1 + 3,
-		"A Y": 2 + 6,
-		"A Z": 3 + 0,
-
-		"B X": 1 + 0,
-		"B Y": 2 + 3,
-		"B Z": 3 + 6,
-
-		"C X": 1 + 6,
-		"C Y": 2 + 0,
-		"C Z": 3 + 3,
-	}
-
 	input, err := os.Open("input")
 	check(err)
 	defer input.Close()
@@ -45,38 +26,6 @@ func challenge01() {
 }
 
 func challenge02() {
-	// Win: 6 - Draw: 3 - Lost: 0
-	// Rock (A - X): 1 - Paper (B - Y): 2 - Scissors (C - Z): 3
-	cheatsheet_old := map[string]int{
-		"A X": 1 + 3,
-		"A Y": 2 + 6,
-		"A Z": 3 + 0,
-
-		"B X": 1 + 0,
-		"B Y": 2 + 3,
-		"B Z": 3 + 6,
-
-		"C X": 1 + 6,
-		"C Y": 2 + 0,
-		"C Z": 3 + 3,
-	}
-
-	// X = loose, Y = draw, Z = win
-	// The idea is that we transform into the old map, to reuse what I did on challenge 01
-	cheatsheet_transformation := map[string]string{
-		"A X": "A Z",
-		"A Y": "A X",
-		"A Z": "A Y",
-
-		"B X": "B X",
-		"B Y": "B Y",
-		"B Z": "B Z",
-
-		"C X": "C Y",
-		"C Y": "C Z",
-		"C Z": "C X",
-	}
-
 	input, err := os.Open("input")
 	check(err)
 	defer input.Close()
@@ -84,10 +33,45 @@ func challenge02() {
 	s := bufio.NewScanner(input)
 	t := 0
 	for s.Scan() {
-		t += cheatsheet_old[cheatsheet_transformation[s.Text()]]
+		t += cheatsheet[cheatsheetTransformation[s.Text()]]
 	}
 
 	fmt.Printf("Challenge 2: %d \n", t)
+}
+
+// Win: 6 - Draw: 3 - Lost: 0
+// Rock (A - X): 1 - Paper (B - Y): 2 - Scissors (C - Z): 3
+// As this has only 9 combinations, we just precalculate them and store it into a matrix.
+// we could do something even better, intead of having this map, having a function that calculates them
+// according to the rules, so is not that hardcoded
+var cheatsheet = map[string]int{
+	"A X": 1 + 3,
+	"A Y": 2 + 6,
+	"A Z": 3 + 0,
+
+	"B X": 1 + 0,
+	"B Y": 2 + 3,
+	"B Z": 3 + 6,
+
+	"C X": 1 + 6,
+	"C Y": 2 + 0,
+	"C Z": 3 + 3,
+}
+
+// X = loose, Y = draw, Z = win
+// The idea is that we transform into the old map, to reuse what I did on challenge 01
+var cheatsheetTransformation = map[string]string{
+	"A X": "A Z",
+	"A Y": "A X",
+	"A Z": "A Y",
+
+	"B X": "B X",
+	"B Y": "B Y",
+	"B Z": "B Z",
+
+	"C X": "C Y",
+	"C Y": "C Z",
+	"C Z": "C X",
 }
 
 // ----------- HELPER ----------//
---
+++ Day03/main.go
@@ -0,0 +1,98 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	challenge01()
+	challenge02()
+}
+
+func challenge01() {
+	input, err := os.Open("Day03/input")
+	check(err)
+	defer input.Close()
+
+	s := bufio.NewScanner(input)
+	var sum int32 = 0
+	for s.Scan() {
+		t := s.Text()
+		first, second := splitString(t)
+		sum += findDuplicatedItem(first, second)
+	}
+	fmt.Printf("Challenge 1: %d \n", sum)
+}
+
+func challenge02() {
+	input, err := os.Open("Day03/input")
+	check(err)
+	defer input.Close()
+
+	s := bufio.NewScanner(input)
+	var sum int32 = 0
+	for s.Scan() {
+		t1 := s.Text()
+		s.Scan()
+		t2 := s.Text()
+		s.Scan()
+		t3 := s.Text()
+
+		m1 := countLetters(t1, make(map[int32]int, 0))
+		m2 := countLetters(t2, make(map[int32]int, 0))
+		m3 := countLetters(t3, make(map[int32]int, 0))
+
+		for index, _ := range m1 {
+			if m2[index] > 0 && m3[index] > 0 {
+				sum += convertToPriority(index)
+			}
+		}
+	}
+
+	fmt.Printf("Challenge 2: %d \n", sum)
+}
+
+func findDuplicatedItem(first string, second string) int32 {
+	m := countLetters(first, make(map[int32]int, 0))
+
+	// now we go to the second
+	for _, c := range second {
+		if m[c] > 0 {
+			// and we only want the one that is missing
+			return convertToPriority(c)
+		}
+	}
+	fmt.Printf("ERROR duplicated not found.\n")
+	return 0
+}
+
+func countLetters(srt string, list map[int32]int) map[int32]int {
+	for _, c := range srt {
+		list[c] += 1
+	}
+	return list
+}
+
+func convertToPriority(c int32) int32 {
+	// Lowercase item types a through z have priorities 1 through 26.
+	// Uppercase item types A through Z have priorities 27 through 52.
+	if c >= 'a' {
+		return c - 96
+	} else {
+		return c - 64 + 26
+	}
+}
+
+func splitString(t string) (string, string) {
+	l := len(t)
+	return t[0 : l/2], t[l/2 : l]
+}
+
+// ----------- HELPER ----------//
+func check(err error) {
+	if err != nil {
+		panic("ERROR: " + err.Error())
+	}
+}
