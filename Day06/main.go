8f1e96d67eac1ada07352199a42481195d4de34c
Leandro Deveikis <leandro.deveikis@gmail.com>
2022-12-08 21:27:58 -0300

    Day06 completed

---
+++ Day06/main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	fmt.Printf("Challenge 1: %d \n", challenge(4))
+	fmt.Printf("Challenge 2: %d \n", challenge(14))
+}
+
+func challenge(length int) int {
+	input, err := os.Open("Day06/input")
+	check(err)
+	defer input.Close()
+
+	s := bufio.NewScanner(input)
+	s.Scan()
+	t := s.Text()
+
+	for i := 0; i < len(t)-length; i++ {
+		subT := t[i : i+length]
+		if areAllCharDifferent(subT) {
+			return i + length
+		}
+	}
+	panic("Result not found")
+}
+
+func areAllCharDifferent(t string) bool {
+	tmp := make(map[int32]int, 0)
+	for _, s := range t {
+		if tmp[s] == 1 {
+			return false
+		} else {
+			tmp[s] = 1
+		}
+	}
+	return true
+}
+
+// ----------- HELPER ----------//
+func check(err error) {
+	if err != nil {
+		panic("ERROR: " + err.Error())
+	}
+}
